12f5c765724a36aee8392f151d000e7fa86f4e9e
Sandro W. Lubis <26489636+sandrolubis@users.noreply.github.com>
2026-09-01 12:49:41 -0700

    Update cal_VKE_budget_MERRA2.py

--- test_MERRA2/cal_VKE_budget_MERRA2.py
+++ test_MERRA2/cal_VKE_budget_MERRA2.py
@@ -12,7 +12,7 @@
 #   Richland, Washington, USA
 #
 # Usage:
-#   python ARanalysisMERRA2.py 20190101 \
+#   python cal_VKE_budget_MERRA2.py 20190101 \
 #       --input-dir /path/to/MERRA2 \
 #       --output-dir ./output
 #
